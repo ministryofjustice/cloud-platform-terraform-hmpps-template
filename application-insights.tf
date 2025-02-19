dcb402d097f1af16230df00b6033f05709b902a5
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2025-02-19 11:52:49 +0000

    Update app insights connection string, include endpoints and app id

--- application-insights.tf
+++ application-insights.tf
@@ -2,6 +2,10 @@ data "aws_ssm_parameter" "application_insights_key" {
   name = "/application_insights/key-${var.application_insights_instance}"
 }
 
+data "aws_ssm_parameter" "application_insights_id" {
+  name = "/application_insights/application-id-${var.application_insights_instance}"
+}
+
 resource "kubernetes_secret" "application-insights" {
   count = contains(["hmpps-template-kotlin", "hmpps-template-typescript"], var.source_template_repo) ? 1 : 0
   metadata {
@@ -13,6 +17,6 @@ resource "kubernetes_secret" "application-insights" {
   }
   data = {
     APPINSIGHTS_INSTRUMENTATIONKEY        = data.aws_ssm_parameter.application_insights_key.value
-    APPLICATIONINSIGHTS_CONNECTION_STRING = "InstrumentationKey=${data.aws_ssm_parameter.application_insights_key.value}"
+    APPLICATIONINSIGHTS_CONNECTION_STRING = "InstrumentationKey=${data.aws_ssm_parameter.application_insights_key.value};IngestionEndpoint=https://northeurope-0.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/;ApplicationId=${data.aws_ssm_parameter.application_insights_id.value}"
   }
 }
